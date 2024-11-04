2e0383ce34e71a3d3eb12faf43eea740febe1d1b
Ganesh Mankar <1gnesh2mankar@gmail.com>
2024-11-04 12:47:00 +0530

    routing improved for production

--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,11 @@ import {  HashRouter } from "react-router-dom";
 import App from "./App.jsx";
 import "./index.css";
 
+const basename = process.env.NODE_ENV === "production" ? "/Stock-Vision" : "";
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <HashRouter>
+    <HashRouter basename={basename}>
       <App />
     </HashRouter>
   </StrictMode>
